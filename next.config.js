f8c9115006b1175473d02e6a6bf37709007536e2
Swaleh Shillah <miclondev@gmail.com>
2025-01-23 20:52:26 +0000

    fix things

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
